f91c9acf26f5a09f031babfcbd8f04985f388e43
Gabriel Isobara <jamaltheatlantean@gmail.com>
2023-01-22 14:59:25 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+A mix of random minimalistic smart contracts that i've studied and developed.
---
+++ contracts/Deed.sol
@@ -0,0 +1,41 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.10;
+
+error Deed__NotOwner();
+error Deed__NotLawyer();
+
+contract Deed {
+    address public owner; // owner of deed
+    address public lawyer; // address of lawyer
+    address private james; // address of inheritor
+    uint public shares; // shares of inheritor
+    uint public timeSpan;
+
+    modifier onlyOwner {
+        if(msg.sender != owner)
+            revert Deed__NotOwner();
+        _;
+    }
+
+    modifier onlyLawyer {
+        if(msg.sender != lawyer)
+            revert Deed__NotLawyer();
+        _;
+    }
+
+    constructor(address _lawyer, address payable _james) {
+        owner = msg.sender;
+        lawyer = _lawyer;
+        james = _james;
+        timeSpan = block.timestamp + 20 days;
+    }
+
+    function expandTimeSpan(uint _newTimeSpan) public onlyOwner {
+        timeSpan = _newTimeSpan;
+    }
+
+    function transfer() public onlyLawyer {
+
+    }
+}
