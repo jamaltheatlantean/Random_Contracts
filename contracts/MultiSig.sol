4ae2d1c3533cfc995e617de708982f094ce72d5d
Gabriel Isobara <jamaltheatlantean@gmail.com>
2023-01-29 06:02:14 +0100

    create events

--- contracts/MultiSig.sol
+++ contracts/MultiSig.sol
@@ -2,5 +2,14 @@
 pragma solidity ^0.8.10;
 
 contract MultiSig {
-    
-}
+
+event DepositConfirmed(address indexed sender, uint amount, uint balance);
+event SubmitTransaction(
+    address indexed owner,
+    uint indexed txIndex,
+    address indexed to,
+    uint value,
+    bytes data
+);
+
+} 
