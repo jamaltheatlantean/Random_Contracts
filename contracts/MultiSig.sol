9d36551ad4a13560c42b5ab28f9e998d4ecd5576
Gabriel Isobara <jamaltheatlantean@gmail.com>
2023-01-30 16:39:25 +0100

    update multisig.sol

--- contracts/MultiSig.sol
+++ contracts/MultiSig.sol
@@ -171,6 +171,10 @@ contract TestContract {
     uint public i;
 
     function callMe(uint j) public {
-        i += j
+        i += j;
+    }
+
+    function getData() public pure returns (bytes memory) {
+        return abi.encodeWithSignature("callMe(uint256)", 123);
     }
 }
