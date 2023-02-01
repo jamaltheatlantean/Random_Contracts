c22e35b47c53171b9c8e15b687fda439ab73378e
Gabriel Isobara <jamaltheatlantean@gmail.com>
2023-02-01 23:10:52 +0100

    update airdrop.sol

--- contracts/Airdrop.sol
+++ contracts/Airdrop.sol
@@ -7,4 +7,7 @@ import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 contract Airdrop {
     address public admin;
     mapping(address => bool) public processedAirdrops;
+    IERC20 public token;
+    uint public currentAirdropAmount;
+    uint public maxAirdropAmount = 10000 * 10 ** 18;
 }
