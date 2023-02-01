d081a419f841b5959653859aa714bbfece3ab59f
Gabriel Isobara <jamaltheatlantean@gmail.com>
2023-02-01 23:07:45 +0100

    create contract add variables

--- contracts/Airdrop.sol
+++ contracts/Airdrop.sol
@@ -3,3 +3,8 @@ pragma solidity 0.8.10;
 
 // Imports
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+
+contract Airdrop {
+    address public admin;
+    mapping(address => bool) public processedAirdrops;
+}
